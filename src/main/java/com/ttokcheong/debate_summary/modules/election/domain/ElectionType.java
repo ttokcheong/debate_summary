6cac6813328793d5dc68ad0d9e44b4562e114880
hainho <hainho9704@gmail.com>
2024-07-22 19:55:19 +0900

    ✨ entity type entity 추가

---
+++ src/main/java/com/ttokcheong/debate_summary/modules/election/domain/ElectionType.java
@@ -0,0 +1,15 @@
+package com.ttokcheong.debate_summary.modules.election.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ElectionType extends AuditingEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+}
