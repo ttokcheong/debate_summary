85b193a3a906e8d23db19dfd244e4994118f80d4
hainho <hainho9704@gmail.com>
2024-07-23 17:18:08 +0900

    ✨ entity type index view에 선거 차수, 선거 일자, 정렬 추가

--- src/main/java/com/ttokcheong/debatesummary/modules/election/controller/EntityTypeController.java
+++ src/main/java/com/ttokcheong/debatesummary/modules/election/controller/EntityTypeController.java
@@ -5,6 +5,8 @@ import com.ttokcheong.debatesummary.modules.election.service.ElectionTypeService
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +19,13 @@ public class EntityTypeController {
   private final ElectionTypeService electionTypeService;
 
   @GetMapping("/election-types")
-  public String index(Model model, @SortDefault("name") Pageable pageable) {
+  public String index(
+      Model model,
+      @SortDefault(
+              sort = {"electionDateTime"},
+              direction = Sort.Direction.DESC)
+          @PageableDefault
+          Pageable pageable) {
     Page<ElectionType> electionTypes = electionTypeService.find(pageable);
     model.addAttribute("electionTypes", electionTypes);
     return "/view/electionType/index";
